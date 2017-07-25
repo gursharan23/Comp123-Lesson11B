0a5ef887291f8041440fc28f0d1014207cf7934b
300931676 <300931676@PR-A315-13>
2017-07-25 17:08:21 -0400

    Created phase enum class

---
+++ Comp123-Lesson11B/Phase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Name : Gursharan Singh
+ * Date : 25 July ,2017
+ * Description : This is the demo class for Lesson 11
+ * Version : 0.1 Created phase enum class
+ */
+namespace Comp123_Lesson11B
+{
+    public enum Phase
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
--- Comp123-Lesson11B/Suit.cs
+++ Comp123-Lesson11B/Suit.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 /*Name : Gursharan Singh
  * Date : 25 July ,2017
  * Description : This is the demo class for Lesson 11
- * Version : 0.1 Created Program class
+ * Version : 0.1 Created Suit enum
  */
 namespace Comp123_Lesson11B
 {
-    class Program
+    public enum Suit
     {
-        static void Main(string[] args)
-        {
-        }
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
     }
 }
